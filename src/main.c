40db79fa68e3ad3fcf15db8496ed8eb25f1f7122
kosmo16 <kosmo.luki@gmail.com>
2013-10-24 13:59:30 +0200

    git

---
+++ src/main.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+ * Includes
+ *****************************************************************************/
+// Libs and api
+#include "../pre_emptive_os/api/osapi.h"
+#include "../pre_emptive_os/api/general.h"
+#include <printf_P.h>
+#include <ea_init.h>
+#include <stdlib.h>
+
+#include "gameboard_keys.h"
+
+/*****************************************************************************
+ *
+ * Description:
+ *    The first function to execute
+ *
+ ****************************************************************************/
+int
+main(void)
+{
+}
